7befd91fc068aa0bf175ccb919b4ed140252fb25
Aniii011 <tiamiyukabirat0@gmail.com>
2026-08-19 04:54:44 +0100

    Create product-option-sets.ts

---
+++ artifacts/nearbuy/src/lib/product-option-sets.ts
@@ -0,0 +1,4 @@
+
+export const COLORS = ["Black", "White", "Red", "Blue", "Green", "Yellow", "Pink", "Purple", "Brown", "Grey", "Orange", "Beige"];
+export const CLOTHING_SIZES = ["XXS", "XS", "S", "M", "L", "XL", "XXL", "3XL"];
+export const SHOE_SIZES = ["36", "37", "38", "39", "40", "41", "42", "43", "44", "45", "46"];
