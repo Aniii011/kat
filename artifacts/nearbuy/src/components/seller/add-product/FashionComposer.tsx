d98a746ecf56050a186e53122dc55792c2dd6e72
Aniii011 <tiamiyukabirat0@gmail.com>
2026-09-18 05:32:50 +0100

    Update FashionComposer.tsx

--- artifacts/nearbuy/src/components/seller/add-product/FashionComposer.tsx
+++ artifacts/nearbuy/src/components/seller/add-product/FashionComposer.tsx
@@ -28,9 +28,9 @@ interface FashionComposerProps {
 export default function FashionComposer(props: FashionComposerProps) {
   return (
     <div className="space-y-5">
-      <TagPicker label="Color" options={FASHION_COLOR_OPTIONS} value={props.color} onChange={(v) => props.onColorChange(v as string)} />
-      <TagPicker label="Size" options={FASHION_SIZE_OPTIONS} value={props.size} onChange={(v) => props.onSizeChange(v as string)} />
-      <TagPicker label="Fit" options={FASHION_FIT_OPTIONS} value={props.fit} onChange={(v) => props.onFitChange(v as string)} />
+      <TagPicker label="Color" options={FASHION_COLOR_OPTIONS} value={props.color} onChange={(v) => props.onColorChange(v as string)} allowCustom />
+      <TagPicker label="Size" options={FASHION_SIZE_OPTIONS} value={props.size} onChange={(v) => props.onSizeChange(v as string)} allowCustom />
+      <TagPicker label="Fit" options={FASHION_FIT_OPTIONS} value={props.fit} onChange={(v) => props.onFitChange(v as string)} allowCustom />
       <TagPicker label="Material" options={FASHION_MATERIAL_OPTIONS} value={props.material} onChange={(v) => props.onMaterialChange(v as string)} allowCustom />
       <TagPicker label="Occasion" options={FASHION_OCCASION_OPTIONS} value={props.occasion} onChange={(v) => props.onOccasionChange(v as string)} />
       <TagPicker label="Audience" options={FASHION_AUDIENCE_OPTIONS} value={props.audience} onChange={(v) => props.onAudienceChange(v as string)} />
